cd95862a3abba7a8142226ed4f68ca4e38dfba8f
Julia Shypat <juliashipat@gmail.com>
2020-02-12 15:35:14 +0100

    Created basic app structure, routings and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { NewsComponent } from './pages/news/news.component';
+import { DepartmentsComponent } from './pages/departments/departments.component';
+import { ToolboxComponent } from './pages/toolbox/toolbox.component';
+import { AnnoncementsComponent } from './pages/annoncements/annoncements.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/toolbox', pathMatch: 'full' },
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'departments', component: DepartmentsComponent },
+  { path: 'toolbox', component: ToolboxComponent },
+  { path: 'annoncements', component: AnnoncementsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NewsComponent,
+    DepartmentsComponent,
+    ToolboxComponent,
+    AnnoncementsComponent,
+    AboutComponent,
+    NotFoundComponent,
+    HeaderComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
